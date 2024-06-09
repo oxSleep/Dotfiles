e304ce9e1323347c6d675e18fb3b9d60a94b1906
oxSleep <99789161+oxSleep@users.noreply.github.com>
2024-06-09 05:00:19 +0000

    Add files via upload

---
+++ config/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+$Env:_ZO_DATA_DIR = "$Env:XDG_CONFIG_HOME\zoxide"
+
+Set-PSReadlineOption -HistorySearchCursorMovesToEnd
+Set-PSReadlineOption -BellStyle None
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -Colors @{
+    Command = 'Yellow'
+    Parameter = 'Green'
+    String = 'DarkCyan'
+}
+
+Set-PSReadLineKeyHandler -Key ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key ctrl+n -Function HistorySearchForward
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
